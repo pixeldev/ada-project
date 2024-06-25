85e754167dc36bb533e93a070b7a3cff338929cb
Angel Miranda <pixeldev1729@gmail.com>
2024-06-25 10:27:04 -0600

    feat: merge it into a single file

--- old/main.cpp
+++ old/main.cpp
@@ -12,17 +12,33 @@
 #include <vector>
 #include <ctime>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
-const int MAXN = 1000;
 const char *RESET_COLOR = "\033[0m";
 const char *GREEN_BACKGROUND = "\033[42m";
 
-int dp[MAXN][MAXN];
-pair<int, int> path[MAXN][MAXN];
-int board[MAXN][MAXN];
+int **dp;
+pair<int, int> **path;
+int **board;
 int n;
+
+void initializeFromFile(const string& filename) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        file >> n;
+        board = new int *[n];
+        for (int i = 0; i < n; i++) {
+            board[i] = new int[n];
+            for (int j = 0; j < n; j++) {
+                file >> board[i][j];
+            }
+        }
+    } else {
+        cout << "No se pudo abrir el archivo.\n";
+    }
+}
 
 int solve(int i, int j) {
     if (i >= n || j >= n) return 0;
@@ -40,20 +56,107 @@ int solve(int i, int j) {
 
 int main() {
     srand(time(0));
-    cout << "Ingrea el tamaño del tablero: \n>> ";
-    cin >> n;
 
-    int min, max;
-    cout << "Ingresa el rango de valores aleatorios: \n>> ";
-    cin >> min >> max;
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
+    cout << "Bienvenido al problema del juego con el mejor puntaje.\n";
+    cout << "El objetivo de este juego es encontrar el camino con la mayor suma de puntos.\n";
+    cout << "Cada celda del tablero tiene un valor aleatorio entre un rango dado.\n";
+    cout << "Se puede mover hacia abajo o hacia la derecha.\n";
+    cout << "-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n";
 
+
+    cout << "¿Cómo quieres llenar el tablero?\n";
+    cout << "1. Llenar con números aleatorios.\n";
+    cout << "2. Llenar manualmente.\n";
+    cout << "3. Usar valores de prueba.\n";
+    cout << "4. Obtener datos desde un archivo.\n>> ";
+
+    int option;
+    cin >> option;
+
+    switch (option) {
+        case 1: {
+            cout << "Ingrea el tamaño del tablero: \n>> ";
+            cin >> n;
+
+            int min, max;
+            cout << "Ingresa el rango de valores aleatorios: \n>> ";
+            cin >> min >> max;
+
+            board = new int *[n];
+            for (int i = 0; i < n; i++) {
+                board[i] = new int[n];
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = rand() % (max - min + 1) + min;
+                }
+            }
+            break;
+        }
+        case 2: {
+            cout << "Ingrea el tamaño del tablero: \n>> ";
+            cin >> n;
+
+            board = new int *[n];
+            for (int i = 0; i < n; i++) {
+                board[i] = new int[n];
+                for (int j = 0; j < n; j++) {
+                    cout << "Ingresa el valor de la celda [" << i << "][" << j << "]: \n>> ";
+                    cin >> board[i][j];
+                }
+            }
+            break;
+        }
+        case 3: {
+            cout << "¿Qué caso de prueba quieres utilizar?.\n";
+            cout << "1. Caso de prueba #1.\n";
+            cout << "2. Caso de prueba #2.\n";
+            cout << "3. Caso de prueba #3.\n>> ";
+            int test;
+            cin >> test;
+            string filename;
+            switch (test) {
+                case 1:
+                    filename = "cases/primer-caso.txt";
+                    break;
+                case 2:
+                    filename = "cases/segundo-caso.txt";
+                    break;
+                case 3:
+                    filename = "cases/tercer-caso.txt";
+                    break;
+                default:
+                    cout << "Opción inválida.\n";
+                    return 1;
+            }
+            initializeFromFile(filename);
+            break;
+        }
+        case 4: {
+            string filename;
+            cout << "Ingresa el nombre del archivo: \n>> ";
+            cin >> filename;
+            initializeFromFile(filename);
+            break;
+        }
+        default:
+            cout << "Opción inválida.\n";
+            return 1;
+    }
+
+    dp = new int *[n];
     for (int i = 0; i < n; i++) {
+        dp[i] = new int[n];
         for (int j = 0; j < n; j++) {
-            board[i][j] = rand() % (max - min + 1) + min;
+            dp[i][j] = -1;
         }
     }
 
-    cout << "Tablero:\n";
+    path = new pair<int, int> *[n];
+    for (int i = 0; i < n; i++) {
+        path[i] = new pair<int, int>[n];
+    }
+
+    cout << "------------------------------------\n" << "Tablero:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             cout << "| " << setw(3) << board[i][j] << " ";
@@ -65,7 +168,6 @@ int main() {
         cout << "+\n";
     }
 
-    memset(dp, -1, sizeof(dp));
     int max_score = -1, max_i = -1, max_j = -1, score = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -77,7 +179,7 @@ int main() {
             }
         }
     }
-    cout << "Máximo puntaje posible: " << max_score << "\nCamino: ";
+    cout << "------------------------------------\n" << "Máximo puntaje posible: " << max_score << "\n\nCamino: \n";
 
     vector<vector<bool>> isPath(n, vector<bool>(n, false));
     int x = max_i, y = max_j;
@@ -86,7 +188,6 @@ int main() {
         tie(x, y) = path[x][y];
     }
 
-    cout << "Tablero:\n";
     for (int i = 0; i < n; ++i) {
         for (int j = 0; j < n; ++j) {
             if (isPath[i][j]) {
@@ -101,5 +202,7 @@ int main() {
         }
         cout << "+\n";
     }
+
+    cout << "------------------------------------\n";
     return 0;
 }
